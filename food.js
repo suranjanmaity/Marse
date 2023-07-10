cb9522de15c1da2efefd529d9c121a7bf3e76431
Suranjan Maity <93034695+suranjanmaity@users.noreply.github.com>
2023-07-10 09:49:33 +0000

    map now works fine

--- food.js
+++ food.js
@@ -1,7 +1,7 @@
-function myMap() {
-    var mapProp= {
-      center:new google.maps.LatLng(51.590980, -0.290670),
-      zoom:5,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    }
+// function myMap() {
+//     var mapProp= {
+//       center:new google.maps.LatLng(51.590980, -0.290670),
+//       zoom:5,
+//     };
+//     var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+//     }
